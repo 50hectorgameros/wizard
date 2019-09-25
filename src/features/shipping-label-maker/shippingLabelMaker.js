cd6f9c94e7d520da139f5d7a06377de5992dcd87
Hector Gameros <hectordgamerosm@gmail.com>
2019-09-25 10:21:53 -0700

    Styled ShippingLabelMaker component.

--- src/features/shipping-label-maker/shippingLabelMaker.js
+++ src/features/shipping-label-maker/shippingLabelMaker.js
@@ -7,6 +7,23 @@ import Confirm from './steps/confirm';
 import GetShippingOption from './steps/getShippingOption';
 import Header from './header';
 import ShippingLabel from './shippingLabel'
+import AppBar from '@material-ui/core/AppBar';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from'@material-ui/icons/ChevronRight';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import NoteAddIcon from '@material-ui/icons/NoteAdd';
 
 const shippingRate = 0.4;
 const ShippingOption = {
@@ -31,6 +48,65 @@ let ShippingInfo = {
 	weight: 0,
 	shippingOption: ''
 };
+const drawerWidth = 200;
+const useStyles = makeStyles( theme => ( {
+	root: {
+		display: 'flex'
+	},
+	title: {
+		flexGrow: 1
+	},
+	appBar: {
+		transition: theme.transitions.create( [ 'margin', 'width' ], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		} )
+	},
+	appBarShift: {
+		width: `calc(100% - ${ drawerWidth }px)`,
+		marginLeft: drawerWidth,
+		transition: theme.transitions.create( [ 'margin', 'width' ], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		} )
+	},
+	menuButton: {
+		marginRight: theme.spacing( 2 )
+	},
+	hide: {
+		display: 'none'
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0
+	},
+	drawerPaper: {
+		width: drawerWidth
+	},
+	drawerHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		padding: theme.spacing( 0, 1 ),
+		...theme.mixins.toolbar,
+		justifyContent: 'flex-end'
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing( 6, 3, 3 ),
+		transition: theme.transitions.create( 'margin', {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		} ),
+		marginLeft: -drawerWidth
+	},
+	contentShift: {
+		transition: theme.transitions.create( 'margin', {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		} ),
+		marginLeft: 0
+	}
+} ) );
 
 function ShippingLabelMaker () {
 	function onComplete () {
@@ -73,8 +149,13 @@ function ShippingLabelMaker () {
 		).toFixed( 2 );
 	}
 
+	const handleDrawerOpen = () => setOpen( true );
+	const handleDrawerClose = () => setOpen( false );
 	const [ labelDone, setLabelDone ] = useState( false );
 	const [ newLabel, setNewLabel ] = useState( false );
+	const [ open, setOpen ] = useState( false );
+	const classes = useStyles();
+	const theme = useTheme();
 
 	if ( labelDone ) {
 		return (
@@ -86,26 +167,79 @@ function ShippingLabelMaker () {
 		);
 	}
 	return (
-		<>
-			{ !newLabel ?
-				<button
-					onClick={ createNewShippingLabel }
-				>
-					Create Shipping Label
-				</button> :
-				<Wizard
-					steps={ [
-						GetSenderAddress,
-						GetReceiverAddress,
-						GetWeight,
-						GetShippingOption,
-						Confirm
-					] }
-					wizardContext={ ShippingInfo }
-					onComplete={ onComplete }
-					header={ Header }
-				/> }
-		</>
+		<div className={ classes.root }>
+			<CssBaseline />
+			<AppBar
+				position='fixed'
+				className={ clsx( classes.appBar, {
+					[ classes.appBarShift ]: open
+				} ) }
+			>
+				<Toolbar>
+					<IconButton
+						edge='start'
+						className={ classes.menuButton }
+						color='inherit'
+						aria-label='open drawer'
+						onClick={ handleDrawerOpen }
+						className={ clsx( classes.menuButton, open && classes.hide ) }
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant='h6' className={ classes.title }>
+						Shipping Label Maker
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Drawer
+				className={ classes.drawer }
+				variant='persistent'
+				anchor='left'
+				open={ open }
+				classes={ {
+					paper: classes.drawerPaper
+				} }
+			>
+				<div className={ classes.drawerHeader }>
+					<IconButton onClick={ handleDrawerClose }>
+						{ theme.direction === 'ltr'
+							? <ChevronLeftIcon />
+							: <ChevronRightIcon /> }
+					</IconButton>
+				</div>
+				<Divider />
+				<List>
+					<ListItem button>
+						<ListItemIcon>
+							<NoteAddIcon />
+						</ListItemIcon>
+						<ListItemText
+							primary='Create Label'
+							onClick={ createNewShippingLabel }
+						/>
+					</ListItem>
+				</List>
+			</Drawer>
+			<main
+				className={ clsx( classes.content, {
+					[ classes.contentShift ]: open
+				} ) }
+			>
+				{ newLabel &&
+					<Wizard
+						steps={ [
+							GetSenderAddress,
+							GetReceiverAddress,
+							GetWeight,
+							GetShippingOption,
+							Confirm
+						] }
+						wizardContext={ ShippingInfo }
+						onComplete={ onComplete }
+						header={ Header }
+					/> }
+			</main>
+		</div>
 	);
 }
 
